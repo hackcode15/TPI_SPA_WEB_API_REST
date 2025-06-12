3ce26f9ac25d975862cdf5e8b6a77d73eaa0f520
Diego <taysonm895@gmail.com>
2025-06-12 19:30:16 -0300

    pongo publico el endpoint de listar profesionales

--- src/main/java/com/app/JWTImplementation/controller/ProfessionalController.java
+++ src/main/java/com/app/JWTImplementation/controller/ProfessionalController.java
@@ -23,7 +23,7 @@ public class ProfessionalController {
 
     @GetMapping("/list")
     @ResponseBody
-    @PreAuthorize("hasRole('ADMIN')")
+    //@PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<List<ProfessionalResponseDTO>>> getAllProfessional(){
         ApiResponse<List<ProfessionalResponseDTO>> response = new ApiResponse<>(
                 "Success",
