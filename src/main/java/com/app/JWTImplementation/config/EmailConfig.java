b161fd5eec598a0a72c95475c46f630e814cd2a1
Diego <taysonm895@gmail.com>
2025-06-13 16:20:24 -0300

    pongo publico el endpoint de professional list

--- src/main/java/com/app/JWTImplementation/config/EmailConfig.java
+++ src/main/java/com/app/JWTImplementation/config/EmailConfig.java
@@ -52,3 +52,41 @@ public class EmailConfig {
     }
 
 }
+
+/*@Configuration
+public class EmailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String starttls;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttls);
+        props.put("mail.smtp.ssl.trust", "*");
+
+        return mailSender;
+    }
+}*/
